07be2ead11bcfc506c3ee8442aaca64bf1da2058
Igor <igorkmail@gmail.com>
2026-01-17 16:43:49 -0300

    adiciona descrição dos treinos no WorkoutSelector

--- src/components/WorkoutSelector.tsx
+++ src/components/WorkoutSelector.tsx
@@ -11,12 +11,14 @@ export default function WorkoutSelector() {
         className="btn btn-lg h-32 bg-base-100 border-2 border-base-content hover:bg-base-300 rounded-lg"
       >
         <span className="text-xl font-bold">TREINO A</span>
+        <div>Upper body e core</div>
       </button>
       <button
         onClick={() => handleSelectWorkout('B')}
         className="btn btn-lg h-32 bg-base-100 border-2 border-base-content hover:bg-base-300 rounded-lg"
       >
         <span className="text-xl font-bold">TREINO B</span>
+        <div>Core e lower body</div>
       </button>
     </div>
   )
