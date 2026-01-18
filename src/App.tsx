24d07c6e5bacda2a19672bfa2e1d5009005e96cb
Igor <igorkmail@gmail.com>
2026-01-18 00:44:20 -0300

    adiciona AnimatePresence para transições de página

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
 import './App.css'
 import HomePage from './pages/HomePage'
 import WorkoutPage from './pages/WorkoutPage'
 
-function App() {
+function AnimatedRoutes() {
+  const location = useLocation()
+
   return (
-    <BrowserRouter>
-      <Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
         <Route path="/" element={<HomePage />} />
         <Route path="/treino/:workout" element={<WorkoutPage />} />
       </Routes>
+    </AnimatePresence>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AnimatedRoutes />
     </BrowserRouter>
   )
 }
